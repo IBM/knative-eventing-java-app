a318553a65c610cdd5d97f3307e8a395ca12585d
Chuck Cox <chuckc@us.ibm.com>
2020-10-01 14:26:40 -0500

    new: ping source test

--- scripts/experience_test.py
+++ scripts/experience_test.py
@@ -1,4 +1,4 @@
-import os, time, sys, datetime
+import os, time, sys, subprocess
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -9,14 +9,15 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
-driver.get(os.environ["APP_URL"] + "/v1/events"); # Open a browser to the app's landing page
+
+subprocess.call("kubectl apply -f ping-source.yaml", shell=True)
+time.sleep(10)
+driver.get(os.environ["APP_URL"] + "/v1/events");  # Open a browser to the app's landing page
 time.sleep(3)
 # Verify the expected content is present
 html = driver.page_source
 print("The page content is: {}".format(html))
-if "No events found in the database!" in html:
+if "Hello world!" in html:
     print("Experience Test Successful")
 else:
     sys.exit("Experience Test Failed")
-
-# TODO start the cron event source and check for events in the events page
