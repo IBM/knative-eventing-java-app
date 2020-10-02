ebcbd1e7642e3853243beaff75c27fa6e69a10a1
Chuck Cox <chuckc@us.ibm.com>
2020-10-02 15:00:59 -0500

    new: cleanup function for all outcomes

--- scripts/experience_test.py
+++ scripts/experience_test.py
@@ -10,6 +10,19 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 
+
+def cleanup():
+    # Cleanup
+    print("Deleting pingsource and clearing the Cloudant DB...")
+    time.sleep(3)  # Adding extra sleeps just so logging of print statements are in order
+    subprocess.call("kubectl delete pingsource cron-pingsource", shell=True)
+    driver.get(os.environ["APP_URL"] + "/v1/events?deleteAll=true");  # Clears the Cloudant DB
+    time.sleep(10)
+    # Verify the event(s) have been cleared
+    html = driver.page_source
+    print("The page content is: {}".format(html))
+
+
 driver.get(os.environ["APP_URL"] + "/v1/events");  # Open a browser to the app's landing page
 time.sleep(3)
 # Verify the expected content is present
@@ -20,7 +33,7 @@ if "No events found" in html:
     print("Starting ping source to send events to the Cloudant DB...")
     time.sleep(3)  # Adding extra sleeps just so logging of print statements are in order
     subprocess.call("kubectl apply -f ping-source.yaml", shell=True)
-    time.sleep(120)
+    time.sleep(60)
     driver.get(os.environ["APP_URL"] + "/v1/events");  # Open a browser to the app's landing page
     time.sleep(3)
     # Verify the expected content is present
@@ -28,17 +41,10 @@ if "No events found" in html:
     print("The page content is: {}".format(html))
     if "Hello world!" in html:
         print("Experience Test Successful")
+        cleanup()
     else:
+        cleanup()
         sys.exit("Experience Test Failed")
 else:
+    cleanup()
     sys.exit("Experience Test Failed")
-
-# Cleanup
-print("Deleting pingsource and clearing the Cloudant DB...")
-time.sleep(3)  # Adding extra sleeps just so logging of print statements are in order
-subprocess.call("kubectl delete pingsource cron-pingsource", shell=True)
-driver.get(os.environ["APP_URL"] + "/v1/events?deleteAll=true");  # Clears the Cloudant DB
-time.sleep(10)
-# Verify the event(s) have been cleared
-html = driver.page_source
-print("The page content is: {}".format(html))
