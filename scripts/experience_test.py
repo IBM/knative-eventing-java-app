e4f45ff6013e7beedde2016f5e693b42f8d574ad
Chuck Cox <chuckc@us.ibm.com>
2020-10-02 13:47:58 -0500

    chore: added 2nd check

--- scripts/experience_test.py
+++ scripts/experience_test.py
@@ -10,20 +10,32 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 
-subprocess.call("kubectl apply -f ping-source.yaml", shell=True)
-time.sleep(60)
 driver.get(os.environ["APP_URL"] + "/v1/events");  # Open a browser to the app's landing page
 time.sleep(3)
 # Verify the expected content is present
 html = driver.page_source
 print("The page content is: {}".format(html))
-if "Hello world!" in html:
-    print("Experience Test Successful")
+if "No events found" in html:
+    print("Confirmed Cloudant DB is empty")
+    print("Starting ping source to send events to the Cloudant DB...")
+    time.sleep(3)  # Adding extra sleeps just so logging of print statements are in order
+    subprocess.call("kubectl apply -f ping-source.yaml", shell=True)
+    time.sleep(30)
+    driver.get(os.environ["APP_URL"] + "/v1/events");  # Open a browser to the app's landing page
+    time.sleep(3)
+    # Verify the expected content is present
+    html = driver.page_source
+    print("The page content is: {}".format(html))
+    if "Hello world!" in html:
+        print("Experience Test Successful")
+    else:
+        sys.exit("Experience Test Failed")
 else:
     sys.exit("Experience Test Failed")
 
 # Cleanup
 print("Deleting pingsource and clearing the Cloudant DB...")
+time.sleep(3)  # Adding extra sleeps just so logging of print statements are in order
 subprocess.call("kubectl delete pingsource cron-pingsource", shell=True)
 driver.get(os.environ["APP_URL"] + "/v1/events?deleteAll=true");  # Clears the Cloudant DB
 time.sleep(10)
